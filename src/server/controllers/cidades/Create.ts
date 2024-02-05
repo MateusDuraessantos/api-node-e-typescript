1622cfef74bfda154fd11e1ed183f5c59acda041
MateusDuraessantos <mateusduraessantos@gmail.com>
2024-02-05 19:26:18 -0300

    aula 9

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from 'express'
-
+import * as yup from 'yup'
 
 interface ICidade {
     nome: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+})
 
-    console.log(req.body)
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validadeData: ICidade | undefined = undefined
+    try {
+        validadeData = await bodyValidation.validate(req.body)
+    } catch (e) {
+        const yupError = e as yup.ValidationError
+        console.log(e);
 
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        })
+    }
+    
+    console.log(validadeData)
+    
     return res.send('Create!')
 }
